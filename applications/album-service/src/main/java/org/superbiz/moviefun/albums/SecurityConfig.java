655de3bb640bf328639318d81b71353796fa81e6
nss_admin <ashwin.gudidevuni@gmail.com>
2018-06-28 09:34:23 -0500

    modified:   applications/album-service/build.gradle 	new file:   applications/album-service/src/main/java/org/superbiz/moviefun/albums/SecurityConfig.java 	modified:   applications/album-service/src/main/resources/application.yml 	modified:   applications/movie-fun-app/build.gradle 	modified:   applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java 	new file:   applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java 	new file:   applications/movie-fun-app/src/main/java/org/superbiz/moviefun/SecurityConfig.java 	modified:   applications/movie-fun-app/src/main/resources/application.yml 	modified:   applications/movie-fun-app/src/main/webapp/WEB-INF/albumDetails.jsp 	modified:   applications/movie-service/build.gradle 	new file:   applications/movie-service/src/main/java/org/superbiz/moviefun/movies/SecurityConfig.java 	modified:   applications/movie-service/src/main/resources/application.yml 	modified:   build.gradle 	new file:   commands627.txt 	modified:   manifest.yml

---
+++ applications/album-service/src/main/java/org/superbiz/moviefun/albums/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.superbiz.moviefun.albums;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.client.RestOperations;
+
+@Configuration
+@EnableResourceServer
+@ConditionalOnProperty(value = "application.oauth-enabled", matchIfMissing = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
+
+
