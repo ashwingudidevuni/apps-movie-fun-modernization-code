28dd297ed06548d42b775a3c0d25e5dd570e30a1
nss_admin <ashwin.gudidevuni@gmail.com>
2018-06-28 15:20:05 -0500

    modified:   applications/movie-fun-app/build.gradle 	modified:   applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java 	modified:   components/movie-fun-api/build.gradle 	modified:   components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/AlbumsController.java 	new file:   components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/CoverCatalog.java 	modified:   manifest.yml

--- components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/AlbumsController.java
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/AlbumsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.superbiz.moviefun.blobstore.Blob;
 import org.superbiz.moviefun.blobstore.BlobStore;
+import sun.tools.asm.Cover;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,11 +27,11 @@ public class AlbumsController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final AlbumsClient albumsClient;
-    private final BlobStore blobStore;
+    private final CoverCatalog coverCatalog;
 
-    public AlbumsController(AlbumsClient albumsClient, BlobStore blobStore) {
+    public AlbumsController(AlbumsClient albumsClient, CoverCatalog coverCatalog) {
         this.albumsClient = albumsClient;
-        this.blobStore = blobStore;
+        this.coverCatalog = coverCatalog;
     }
 
     @GetMapping
@@ -47,11 +48,11 @@ public class AlbumsController {
 
     @PostMapping("/{albumId}/cover")
     public String uploadCover(@PathVariable Long albumId, @RequestParam("file") MultipartFile uploadedFile) {
-        logger.debug("Uploading cover for album with id {}", albumId);
-
         if (uploadedFile.getSize() > 0) {
             try {
-                tryToUploadCover(albumId, uploadedFile);
+                coverCatalog.uploadCover(albumId, uploadedFile.getInputStream(), uploadedFile.getContentType());
+                logger.debug("Uploaded cover for album with id {}", albumId);
+
 
             } catch (IOException e) {
                 logger.warn("Error while uploading album cover", e);
@@ -63,8 +64,7 @@ public class AlbumsController {
 
     @GetMapping("/{albumId}/cover")
     public HttpEntity<byte[]> getCover(@PathVariable long albumId) throws IOException, URISyntaxException {
-        Optional<Blob> maybeCoverBlob = blobStore.get(getCoverBlobName(albumId));
-        Blob coverBlob = maybeCoverBlob.orElseGet(this::buildDefaultCoverBlob);
+        Blob coverBlob = coverCatalog.getCover(albumId);
 
         byte[] imageBytes = IOUtils.toByteArray(coverBlob.inputStream);
 
@@ -75,25 +75,4 @@ public class AlbumsController {
         return new HttpEntity<>(imageBytes, headers);
     }
 
-
-    private void tryToUploadCover(@PathVariable Long albumId, @RequestParam("file") MultipartFile uploadedFile) throws IOException {
-        Blob coverBlob = new Blob(
-            getCoverBlobName(albumId),
-            uploadedFile.getInputStream(),
-            uploadedFile.getContentType()
-        );
-
-        blobStore.put(coverBlob);
-    }
-
-    private Blob buildDefaultCoverBlob() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream input = classLoader.getResourceAsStream("default-cover.jpg");
-
-        return new Blob("default-cover", input, MediaType.IMAGE_JPEG_VALUE);
-    }
-
-    private String getCoverBlobName(@PathVariable long albumId) {
-        return format("covers/%d", albumId);
-    }
 }
